b0ec8b133a40798b611cb66d5b653ac9f5326a82
Yuval <yuvalvarshevsky@gmail.com>
2022-01-19 01:46:55 +0200

    add

--- routes/users.ts
+++ routes/users.ts
@@ -37,7 +37,7 @@ router.post(
       let user = await UserModel.findOne({ email });
 
       if (user) {
-        res.status(400).json({ msg: 'User already exists' });
+        return res.status(400).json({ msg: 'User already exists' });
       }
 
       user = new UserModel<User>({
