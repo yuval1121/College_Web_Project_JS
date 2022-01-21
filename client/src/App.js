8ab7a28827cb94865cfe37e088faddde6987451a
Yuval <yuvalvarshevsky@gmail.com>
2022-01-21 13:14:30 +0200

    added basic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar />
-      My app
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </>
+    </Router>
   );
 };
 
